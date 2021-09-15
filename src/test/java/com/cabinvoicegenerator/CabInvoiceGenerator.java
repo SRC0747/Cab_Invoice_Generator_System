2e41c4569ed2b3e68aa9f9b56ef06e7e378ca381
SRC0747 <RjSrc@0747>
2021-09-15 13:19:39 +0530

    added: Calculating Fare based on for how long how much distance is travelled.

--- src/test/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,7 +1,23 @@
 package com.cabinvoicegenerator;
 
+import java.util.*;
+
 public class CabInvoiceGenerator {
+    public static void calculateFare(){
+        double dis = 0,time = 0,cost = 0;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the Distance in Killometer:");
+        double distance = sc.nextDouble();
+        System.out.print("Enter Time:");
+        double t = sc.nextDouble();
+        dis = (dis + distance*10);
+        time = (time + t*1);
+        cost = (cost + distance + t);
+        System.out.println("Total Cost is:"+cost);
+    }
     public static void main(String[] args){
-        System.out.println("Welcome to Cab Invoice Generator probblem.");
+        System.out.println("Welcome to Cab Invoice Generator problem.");
+        CabInvoiceGenerator obj = new CabInvoiceGenerator();
+        obj.calculateFare();
     }
 }
---
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.cabinvoicegenerator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CabInvoiceGeneratorTest {
+    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+    @Test
+    public void toCalculate_Fare() {
+        cabInvoiceGenerator.calculateFare();
+    }
+}
